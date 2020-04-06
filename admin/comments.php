f1576c87d2d1d18c2ba19e5ef27aba8834ce5794
YoselinGT <yoselin.galvan.torres@gmail.com>
2020-04-05 19:20:20 -0500

    Page comments

--- admin/comments.php
+++ admin/comments.php
@@ -1,6 +1,10 @@
 <?php include("includes/header.php"); ?>
-<?php include("includes/navigation.php"); ?>
-
+<?php include("includes/navigation.php"); 
+if(!$session->is_signed_in()){
+    redirect("login.php");
+}
+$comments = Comment::find_all();
+?>
         <div id="page-wrapper">
 
             <div class="container-fluid">
@@ -10,16 +14,42 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Comments
-                            <small>Subheading</small>
                         </h1>
+                        <p class="bg-success"><?php echo $message; ?></p>
                         <ol class="breadcrumb">
                             <li>
                                 <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
                             </li>
                             <li class="active">
-                                <i class="fa fa-file"></i> Comments
+                                <i class="fa fa-file"></i> comments
                             </li>
                         </ol>
+                        <div class="col-md-12">
+                            <table  class="table table-hover">
+                               <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Author</th>
+                                        <th>Body</th>
+                                    </tr>
+                               </thead>
+                               <tbody>
+                               <?php foreach($comments as $comment):?>
+                               
+                                    <tr>
+                                        <td><?php echo $comment->id; ?></td>
+                                        <td><?php echo $comment->author; ?>
+                                            <div class="pictures_link">
+                                                <a href="delete_comment.php?id=<?php echo $comment->id; ?>">Delete</a>
+                                            </div>
+                                        </td>
+                                        <td><?php echo $comment->body; ?></td>
+                                    </tr>
+                               <?php endforeach;?>
+                               </tbody>
+                            </table>
+                        </div>
+                        
                     </div>
                 </div>
                 <!-- /.row -->
